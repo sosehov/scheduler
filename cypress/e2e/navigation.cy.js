43ae6e2377695ffcd46a4a15d2bd399f23658609
Soseh <sosehv@gmail.com>
2025-04-29 15:32:24 -0400

    Updates Cypress config.js and adds cypress folder and its e2e tests

---
+++ cypress/e2e/navigation.cy.js
@@ -0,0 +1,12 @@
+describe('Navigation', () => {
+  it('should visit root', () => {
+    cy.visit('/');
+  })
+
+  it('should navigate to Tuesday', () => {
+    cy.visit('/');
+    cy.get("li").contains("Tuesday").click()
+    cy.contains('[data-testid="day"]', "Tuesday")
+    .should("have.class", "day-list__item--selected");
+  })
+});
